d0359e20875821a584db0a8142f38f719c4440a2
junaid9011 <junaid.cou@gmail.com>
2025-04-17 09:09:16 +0600

    done

--- Utlis/sendToken.js
+++ Utlis/sendToken.js
@@ -1,17 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const sendToken = (user, statusCode, res) => {
-    const accessToken = user.getJwtToken();
-    
+    const accessToken = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRE_TIME } // '15m'
+    );
+
     const refreshToken = jwt.sign(
         { id: user._id },
         process.env.JWT_REFRESH_SECRET,
-        { expiresIn: process.env.JWT_REFRESH_EXPIRES_TIME }
+        { expiresIn: process.env.JWT_REFRESH_EXPIRES_TIME } // '7d'
     );
 
     const cookieOptions = {
         expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+            Date.now() + 7 * 24 * 60 * 60 * 1000 // 7 days in ms
         ),
         httpOnly: true,
         secure: process.env.NODE_ENV === 'prod',
